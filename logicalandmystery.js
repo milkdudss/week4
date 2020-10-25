c45c76995945be162a9860fdb7ca78e754765ce7
milkdudss <albertkoshy@gmail.com>
2020-10-24 19:50:56 -0700

    w

---
+++ logicalandmystery.js
@@ -0,0 +1,7 @@
+const mystery = 'P234243-17'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
